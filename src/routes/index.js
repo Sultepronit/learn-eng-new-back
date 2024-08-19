2350e29bfcd56c6bf3d74e6356060975e9d23bbb
Sultepronit <sultepronit@gmail.com>
2024-08-19 08:06:14 +0300

    added patch method quick handler

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,13 +13,17 @@ router.get('/words', async (req, res) => {
     }
 });
 
-// router.get('/node-learn-eng/words', async (req, res) => {
-//     try {    
-//         res.json(await getWords(true, true));
-//     } catch (error) {
-//         res.status(400).json({ 'error': error.message });
-//     }
-// });
+router.patch('/words/*', async (req, res) => {
+    try {
+        // res.json(req.query);
+        // setTimeout(() => {
+        //     res.json({ id: req.params[0] });
+        // }, 5000);
+        res.json({ id: req.params[0] });
+    } catch (error) {
+        res.status(400).json({ 'error': error.message });
+    }
+});
 
 router.get('*', (req, res) => {
     res.send(req.params);
