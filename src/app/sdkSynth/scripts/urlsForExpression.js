29b6038337dfd9d7cdcc609503d7054d1b2204e6
Sultepronit <sultepronit@gmail.com>
2024-08-28 15:09:09 +0300

    little change

--- src/app/sdkSynth/scripts/urlsForExpression.js
+++ src/app/sdkSynth/scripts/urlsForExpression.js
@@ -1,4 +1,5 @@
-const speakers = ['m2', 'f1', 'f2', 'm1'];
+// const speakers = ['m2', 'f1', 'f2', 'm1'];
+const speakers = ['m2', 'f2', 'm1', 'f1'];
 
 export default function urlsForExpression(expression, temp = false) {
     let urlBase = 'https://dthjysrf.duckdns.org/node-learn-eng/audio';
