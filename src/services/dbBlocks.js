d546ec73528df1647c00099a92859a21a21ca79a
Sultepronit <sultepronit@gmail.com>
2024-09-22 21:25:41 +0300

    refactored; cleaned

--- src/services/dbBlocks.js
+++ src/services/dbBlocks.js
@@ -1,13 +1,10 @@
 const dbBlocks = {
     articles: {
-        db: ['number', 'word', 'transcription', 'translation', 'example'],
-        get js() {
-            return this.db;
-        }
+        db: ['number', 'tap_status', 'word', 'transcription', 'translation', 'example'],
+        js: ['number', 'repeatStatus', 'word', 'transcription', 'translation', 'example'],
     },
     tap: {
         db: [
-            'tap_status', 
             'tap_f_progress', 
             'tap_f_record', 
             'tap_f_autorepeat', 
@@ -48,30 +45,3 @@ const dbBlocks = {
 };
 
 export default dbBlocks;
-
-// const dbBlocks = {
-//     articles: [
-//         ['word', 'word'],
-//         ['transcription', 'transcription'],
-//         ['translation', 'translation'],
-//         ['example', 'example']
-//     ],
-//     tap: [
-//         ['tap_status', 'tapStatus'],
-//         ['tap_f_progress', 'tapFProgress'],
-//         ['tap_f_record', 'tapFRecord'],
-//         ['tap_f_autorepeat', 'tapFAutorepeat'],
-//         ['tap_b_progress', 'tapBProgress'],
-//         ['tap_b_record', 'tapBRecord'],
-//         ['tap_b_autorepeat', 'tapBAutorepeat']
-//     ],
-//     write: [
-//         ['write_status', 'writeStatus'],
-//         ['write_f_progress', 'writeFProgress'],
-//         ['write_f_record', 'writeFRecord'],
-//         ['write_f_autorepeat', 'writeFAutorepeat'],
-//         ['write_b_progress', 'writeBProgress'],
-//         ['write_b_record', 'writeBRecord'],
-//         ['write_b_autorepeat', 'writeBAutorepeat']
-//     ]
-// };
